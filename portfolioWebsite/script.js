e297b915a53c1096ef154cf842948b94afb05bb3
Anmol0709s <anmolkumar07sept@gmail.com>
2022-08-04 06:08:25 -0700

    first commit

---
+++ portfolioWebsite/script.js
@@ -0,0 +1,6 @@
+console.log("hiiiiiiiiiiiiii");
+
+document.querySelector('.cross').style.display = 'none';
+document.querySelector('.haim').addEventListener("click" , ()=>{
+document.querySelector('.sidebar').classList.toggle('sidebarGo')
+});
